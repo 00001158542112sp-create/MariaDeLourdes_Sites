0366da6a65e99a68387514d128709eefc3d06c4f
00001158542112sp-create <00001158542112sp@al.educacao.sp.gov.br>
2026-09-14 10:39:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-document.open();
-document.write('');
-document.close();
-document.open();
+// Aguarda o carregamento do documento
+document.addEventListener('DOMContentLoaded', () => {
+    const botaoSites = document.getElementById('btn-sites');
+
+    // Exemplo de interação: Log no console quando o botão for clicado
+    botaoSites.addEventListener('click', (event) => {
+        console.log('O usuário está sendo redirecionado para a página de Sites...');
+    });
+});
